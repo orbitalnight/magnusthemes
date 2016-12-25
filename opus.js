fe2309e68eef46cc3f172967444fd9626c194d1b
orbitalnight <bevswagmagnus@gmail.com>
2016-12-25 12:13:43 +0800

    Add files via upload

---
+++ opus.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+   $('body').append('<div id="opus"><div id="jan"><a href="http://magnusthemes.tumblr.com" title="theme by magnusthemes"><img src="http://static.tumblr.com/p0knose/pyanklckv/vers.png" width="25"></a></div></div>');
+});
