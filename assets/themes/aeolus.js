8d1d3eedbd9287c7d2c2eb83ac8cc3592d57fe92
orbitalnight <24759950+orbitalnight@users.noreply.github.com>
2019-03-06 00:24:58 +0800

    Create aeolus.js

---
+++ assets/themes/aeolus.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    $("[title]").style_my_tooltips({
+        tip_follows_cursor:true,
+        tip_delay_time:100,
+        tip_fade_speed:250,
+        attribute:"title"
+    });
+    $(".ftr").each(function(){
+        let $this = $(this);
+        let img = $this.find("img")[0],
+            imgSrc = img.src;
+        $this.css({
+            "background":"url("+imgSrc+")",
+            "background-size":"cover"
+        });
+        img.parentNode.removeChild(img);
+    });
+    $(".bars .b").each(function(){
+        var $this = $(this);
+        var $percentage = $this.children("span"),
+            percent = parseInt($percentage[0].textContent);
+        var totalWidth = $this.innerWidth();
+        $percentage.css("width",percent*totalWidth/100+"px");
+        $percentage[0].textContent = "";
+    });
+    var a = document.createElement('a');
+    a.innerHTML = "theme";
+    a.title = "theme by magnusthemes";
+    a.href = "http://magnusthemes.tumblr.com";
+    document.getElementById("links").appendChild(a);
+});
