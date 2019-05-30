212ebdd4e1970385510c0cf1ebc448c2bb50cee9
orbitalnight <24759950+orbitalnight@users.noreply.github.com>
2019-05-31 03:58:46 +0800

    Create player-basic.js

---
+++ assets/main/player-basic.js
@@ -0,0 +1,28 @@
+feather.replace();
+function playMusic(audio,start,stop){
+	audio.play();
+    $(start).hide();
+    $(stop).show();
+}
+function pauseMusic(audio,start,stop){
+	audio.pause();
+    $(start).show();
+    $(stop).hide();
+}
+function checkStop(stopped){
+	if(!stopped)
+    	pauseMusic(aud,start,stop);
+    else
+    	playMusic(aud,start,stop);
+}
+var player = document.getElementById("player"),
+	aud = player.getElementsByTagName("audio")[0],
+	start = player.getElementsByClassName("start")[0],
+    stop = player.getElementsByClassName("stop")[0];
+var stopped = 0; // audio paused - true
+checkStop(stopped);
+stopped = 1;
+$("#switch").on("click",function(){
+	checkStop(stopped);
+    stopped = !stopped;
+});
